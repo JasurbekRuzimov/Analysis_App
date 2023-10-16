1e609944ec1912ff070705f7929a0c692b3ff7ef
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-10-16 16:19:33 +0500

    Combinatoric algorithm added

--- app/src/main/java/uz/jasurbekruzimov/randomapp/GameAdapter.java
+++ app/src/main/java/uz/jasurbekruzimov/randomapp/GameAdapter.java
@@ -1,37 +1,34 @@
 package uz.jasurbekruzimov.randomapp;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
 
 public class GameAdapter extends RecyclerView.Adapter<GameAdapter.GameViewHolder> {
-    private List<Game> gamesList;
+    private List<String> gamesList;
 
-    public GameAdapter(List<Game> gamesList) {
+    public GameAdapter(List<String> gamesList) {
         this.gamesList = gamesList;
     }
 
     @NonNull
     @Override
     public GameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View gameView = inflater.inflate(R.layout.my_row, parent, false);
-        return new GameViewHolder(gameView);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_game, parent, false);
+        return new GameViewHolder(view);
     }
 
-    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull GameViewHolder holder, int position) {
-        Game game = gamesList.get(position);
-        holder.numId.setText(String.valueOf(position + 1));
-        holder.yes_txt.setText("Variant-" + String.valueOf(position+1));
+        String game = gamesList.get(position);
+        holder.variantNumber.setText("Variant raqami: " + (position + 1));
+        holder.resultTextView.setText(game);
     }
 
     @Override
@@ -39,14 +36,14 @@ public class GameAdapter extends RecyclerView.Adapter<GameAdapter.GameViewHolder
         return gamesList.size();
     }
 
-    static class GameViewHolder extends RecyclerView.ViewHolder {
-        TextView numId;
-        TextView yes_txt;
+    public static class GameViewHolder extends RecyclerView.ViewHolder {
+        TextView variantNumber;
+        TextView resultTextView;
 
-        GameViewHolder(View itemView) {
+        public GameViewHolder(@NonNull View itemView) {
             super(itemView);
-            numId = itemView.findViewById(R.id.numId);
-            yes_txt = itemView.findViewById(R.id.yes_txt);
+            variantNumber = itemView.findViewById(R.id.variantNumber);
+            resultTextView = itemView.findViewById(R.id.textView);
         }
     }
 }
